6e0ff920c7ac2aba19939300283caa57d12a5dba
Simon Meoni <simonmeoni@aol.com>
2016-06-07 16:46:02 +0200

    add new example and implementation of xmllint

--- main.rb
+++ main.rb
@@ -27,9 +27,42 @@ def create_error_dict (_list)
     end
   end
   return error
+end
+
+def xmllint_error (_file,_error_dict)
+  list = []
+  File.open(_file).each do |line|
+    if (!line.include?("^\n"))
+      list << line.chomp
+    end
   end
+
+  list = list.values_at(* list.each_index.select {|i| i.even?})
+  list.each do |el|
+    split_el = el.split(" ")
+    type = el[/element|attribute/]
+    key = type + ";" + el.split(":")[-1]
+    filename =  File.basename(split_el[0])[0..-2]
+    if (_error_dict.has_key?(key))
+      _error_dict[key] <<  " || " + filename
+    else
+      _error_dict[key] = filename
+    end
+  end
+end
+
+rnv_arg = ARGV.shift
+xmllint_arg = ARGV.shift
+raise "missing argument" unless rnv_arg
+
 f = File.new("error.csv",  "w+")
-f.write("type; error; files \n")
-create_error_dict(fill_list(ARGV[0])).each do |entry|
-  f.write(entry.join("; ")[0..-2] + "\n" )
+f.write("type; error; freq; files \n")
+error = create_error_dict(fill_list(rnv_arg))
+
+if (xmllint_arg != nil)
+  xmllint_error(xmllint_arg, error)
+end
+error.each do |entry|
+  freq = entry[1].split("||").length.to_s
+  f.write(entry.insert(1, freq).join("; ")[0..-2] +  "\n" )
 end
